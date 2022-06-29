b9bf180d925afa971cb09eccd74dfa58ecb13129
Alex Carthew <alexcarthew77@googlemail.com>
2022-06-29 16:23:44 +0100

    added prod auth_URI

--- src/spotify.js
+++ src/spotify.js
@@ -1,5 +1,5 @@
-//const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize?client_id=337e3bf90cf94939a4f39b4bb4eca5ec&response_type=token&redirect_uri=https://spotifest-944cc.web.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize?client_id=337e3bf90cf94939a4f39b4bb4eca5ec&response_type=token&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize?client_id=337e3bf90cf94939a4f39b4bb4eca5ec&response_type=token&redirect_uri=https://spotifest-944cc.web.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+//const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize?client_id=337e3bf90cf94939a4f39b4bb4eca5ec&response_type=token&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 const redirect_uri = "http://localhost:3000/"
 const client_id = "337e3bf90cf94939a4f39b4bb4eca5ec"
 //https://spotifest-944cc.web.app/
