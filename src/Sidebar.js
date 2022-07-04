0b7440a50aa17d8908b9daf3dfcb0af964cb6149
Alex Carthew <alexcarthew77@googlemail.com>
2022-07-04 16:04:01 +0100

    Added collapse when click sidebar option on mobile

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,22 +12,29 @@ import { style } from '@mui/system';
 
 function Sidebar({ spotify }) {
 
-    const [{ playlists, isExpanded }] = useDataLayerValue();
+    const [{ playlists, isExpanded }, dispatch] = useDataLayerValue();
+
+    function toggleSidebarShow() {
+        dispatch({
+            type: 'SET_IS_EXPANDED',
+            isExpanded: !isExpanded
+        })
+    }
 
     return (
         <div className={isExpanded ? 'Sidebar' : 'Sidebar.expanded'}>
             <img className='SidebarLogo' alt='logo' src={logo} />
             {/* <img className='SidebarLogo' alt='logo' src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg' /> */}
-            <Link to='/' style={{ textDecoration: 'none' }}><SidebarOption title="Home" Icon={HomeIcon} /></Link>
-            <Link to='/search' style={{ textDecoration: 'none' }}><SidebarOption title="Search" Icon={SearchOutlinedIcon}/> </Link>
-            <Link to='/festival' style={{ textDecoration: 'none' }}><SidebarOption title="Create Festival Playlist" Icon={FestivalIcon}/></Link>
+            <Link to='/' style={{ textDecoration: 'none' }}><SidebarOption title="Home" Icon={HomeIcon} onClick={() => toggleSidebarShow()} /></Link>
+            <Link to='/search' style={{ textDecoration: 'none' }}><SidebarOption title="Search" Icon={SearchOutlinedIcon} onClick={() => toggleSidebarShow()}/> </Link>
+            <Link to='/festival' style={{ textDecoration: 'none' }}><SidebarOption title="Create Festival Playlist" Icon={FestivalIcon} onClick={() => toggleSidebarShow()}/></Link>
 
             <br />
             <strong className='SidebarTitle'>PLAYLISTS</strong>
             <hr />
 
             {playlists?.items?.map(playlist => (
-                <SidebarOption title={playlist.name} uri={playlist.uri} playlist={playlist} />
+                <SidebarOption title={playlist.name} uri={playlist.uri} playlist={playlist} onClick={() => toggleSidebarShow()} />
             ))}
 
         </div>
